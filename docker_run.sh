f4ac90e7401048486a96053726d2b95fa993e79f
webf5 <591454723@qq.com>
2020-06-09 01:38:52 +0800

    3commit

--- docker_run.sh
+++ docker_run.sh
@@ -1,2 +1,11 @@
 #!/bin/bash
+curDir=$(cd `dirname $0`; pwd)
+
+export curDir
+# export curDir=$(pwd)
+
 /usr/local/bin/docker-compose -f docker-compose_zabbix.yml up -d
+#echo ${curDir} 
+#echo $dir
+#echo $dirCur
+
